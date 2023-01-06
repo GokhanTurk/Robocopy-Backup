1241f3a52472ff4fb693fbb6b0769d37f5dfb5af
Gökhan Türk <ensar.gokhan@hotmail.com>
2023-01-06 11:21:11 +0300

    Update

--- Backup.ps1
+++ Backup.ps1
@@ -3,9 +3,16 @@ try {
     [string[]]$confirm
     $inputWarning = "INPUTS MUST BE A NUMBER!"
     Clear-Host
+    Function Explanation{
+        Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ROBOCOPY-BACKUP~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n`n"
+        Write-Host "1) This option will copy the following folders of selected user to selected destination: Desktop, Documents, Downloads, Videos, Pictures, Music Google Chrome user data`n"
+        Write-Host "2) If the backup was taken with this script, using this option, the data in the backup folder you selected will be copied to the folders specified in the top line.`n"
+        Write-Host "3) This option; copies a selected folder (this could be the entire disk) to the specified folder.`n`n"
+        Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n`n"
+    }
     Function Select-Action {
+        Explanation
         Write-Host "1) Backup User Data" -BackgroundColor DarkGreen -ForegroundColor White
-        Write-Host "(It will copy the following folders of selected user: Desktop, Documents, Downloads, Google Chrome user data)" -BackgroundColor DarkGreen -ForegroundColor White
         Write-Host "2) Recover User Data" -BackgroundColor White -ForegroundColor Black 
         Write-Host "3) Backup a Folder or Entire Disk" -BackgroundColor DarkBlue -ForegroundColor White
         Write-Host "4) Exit" -BackgroundColor DarkRed -ForegroundColor White
